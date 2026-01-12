1d1e214bc35d0b83481d3df2b126c90e2c09261e
imanehajji-tech <imane.hajji@3wa.io>
2026-01-12 09:38:47 +0000

    Exercice 1 : ajouter à la fin (HTML en Javascript)

---
+++ exercice-1/index.js
@@ -0,0 +1,6 @@
+let list = document.querySelector("ul");
+let item = document.createElement("li");
+list.appendChild(item);
+
+let itemText = document.createTextNode("Beurre");
+item.appendChild(itemText);
